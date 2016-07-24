42ee37019358c0c57d417253a6bf4c4b2d5b228f
Denisse Lara <dmlaramartin@gmail.com>
2016-07-23 20:10:03 -0400

    Remove stats

--- main.js
+++ main.js
@@ -8,12 +8,11 @@ cc.game.onStart = function(){
     cc.view.adjustViewPort(true);
     // Setup the resolution policy and design resolution size
     cc.view.setDesignResolutionSize(1242, 2208, cc.ResolutionPolicy.SHOW_ALL);
-    // Instead of set design resolution, you can also set the real pixel resolution size
-    // Uncomment the following line and delete the previous line.
-    // cc.view.setRealPixelResolution(960, 640, cc.ResolutionPolicy.SHOW_ALL);
     // The game will be resized when browser size change
     cc.view.resizeWithBrowserSize(true);
-    //load resources
+    // remove stats
+    cc.director.setDisplayStats(false);
+    // load resources
     cc.LoaderScene.preload(g_resources, function () {
         cc.spriteFrameCache.addSpriteFrames(
             petAnimationsRes.pet_standby_plist
